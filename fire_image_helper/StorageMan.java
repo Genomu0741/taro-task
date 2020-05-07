ab80566a61a20d5b1dacec81e67c56e79de6cdd7
Genomu <B07505019@ntu.edu.tw>
2020-05-07 14:09:37 +0800

    fire_image_helper

---
+++ fire_image_helper/StorageMan.java
@@ -0,0 +1,23 @@
+package genomu.fire_image_helper;
+
+import android.app.Activity;
+
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.UploadTask;
+
+public class StorageMan {
+    protected String storeKey;
+    protected Activity activity;
+    protected FaFa faFa;
+    protected OnProgressListener<? super UploadTask.TaskSnapshot> progressListener;
+
+    public StorageMan(String storeKey, Activity activity) {
+        this.storeKey = storeKey;
+        this.activity = activity;
+        faFa = new FaFa(storeKey);
+    }
+
+    public void setOnProgressListener(OnProgressListener<? super UploadTask.TaskSnapshot> progressListener) {
+        this.progressListener = progressListener;
+    }
+}
