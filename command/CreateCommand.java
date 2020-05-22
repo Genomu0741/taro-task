2498283cfb0d2b370248a315757c892e5970c4af
Genomu0741 <b07505019@g.ntu.edu.tw>
2020-05-22 19:37:30 +0800

    CreateCommand update

--- command/CreateCommand.java
+++ command/CreateCommand.java
@@ -1,16 +1,23 @@
 package genomu.command;
 
 import android.app.Activity;
+import android.content.Intent;
 
 import androidx.annotation.NonNull;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.Query;
+
+import java.io.Serializable;
 
 import genomu.firestore_helper.DBCommand;
+import genomu.firestore_helper.DBEmcee;
+import genomu.firestore_helper.DBReceiver;
 
-public class CreateCommand extends DBCommand {
+public class CreateCommand extends DBCommand implements DBEmcee {
     private Object POJO;
     public CreateCommand(String root, Activity activity, Object POJO) {
         super(root,activity);
@@ -31,6 +38,27 @@ public class CreateCommand extends DBCommand {
                 if(task.isSuccessful()){
                     String id = task.getResult().getId();
                     task.getResult().update("id",id);
+                    goLive(task.getResult().get());
+                }
+            }
+        });
+    }
+
+    @Override
+    public void goLive(Query query) {
+
+    }
+
+    @Override
+    public void goLive(Task<DocumentSnapshot> snapshotTask) {
+        snapshotTask.addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if(task.isSuccessful()){
+                    Serializable item = (Serializable) task.getResult().toObject(POJO.getClass());
+                    Intent intent = new Intent(ACTION01);
+                    intent.putExtra(DBReceiver.DES_POJO,item);
+                    activity.sendBroadcast(intent);
                 }
             }
         });
